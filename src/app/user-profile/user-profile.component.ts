b5ffebc8779036f1f02372346fc3eb77c28115ee
mickael robert <dinak77240@gmail.com>
2019-04-01 18:01:34 +0200

    fix beug user-profile

--- src/app/user-profile/user-profile.component.ts
+++ src/app/user-profile/user-profile.component.ts
@@ -1,6 +1,6 @@
 import { Component, EventEmitter, Input, Output, OnInit } from '@angular/core';
-
 import { User } from './user';
+
 @Component({
   selector: 'app-user-profile',
   templateUrl: './user-profile.component.html',
